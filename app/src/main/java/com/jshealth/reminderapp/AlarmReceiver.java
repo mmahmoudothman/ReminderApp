de7889a354bd624ccf7ecef9af34643087a9cdbb
osos <mahmoudosman@paymobsolutions.com>
2020-09-13 23:49:46 +0200

    Init Project

---
+++ app/src/main/java/com/jshealth/reminderapp/AlarmReceiver.java
@@ -0,0 +1,174 @@
+
+
+package com.jshealth.reminderapp;
+
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.legacy.content.WakefulBroadcastReceiver;
+
+import java.util.Calendar;
+
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+    AlarmManager mAlarmManager;
+    PendingIntent mPendingIntent;
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.Builder mBuilder;
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int mReceivedID = Integer.parseInt(intent.getStringExtra(ReminderEditActivity.EXTRA_REMINDER_ID));
+        Log.e("test", "=>>>>>@@====  onReceive@@@@@ >>>>>>>>>");
+        // Get notification title from Reminder Database
+        ReminderDatabase rb = new ReminderDatabase(context);
+        Reminder reminder = rb.getReminder(mReceivedID);
+        String mTitle = reminder.getTitle();
+
+        // Create intent to open ReminderEditActivity on notification click
+        Intent editIntent = new Intent(context, ReminderEditActivity.class);
+        editIntent.putExtra(ReminderEditActivity.EXTRA_REMINDER_ID, Integer.toString(mReceivedID));
+        PendingIntent mClick = PendingIntent.getActivity(context, mReceivedID, editIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder = new NotificationCompat.Builder(context);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setContentTitle(context.getResources().getString(R.string.app_name))
+                .setContentText(mTitle)
+                .setAutoCancel(false)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setContentIntent(mClick);
+
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            assert mNotificationManager != null;
+            mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        assert mNotificationManager != null;
+        mNotificationManager.notify(0 /* Request Code */, mBuilder.build());
+
+/*
+        NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String channelId = "some_channel_id";
+            CharSequence channelName = "Some Channel";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel notificationChannel = null;
+            notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            nManager.createNotificationChannel(notificationChannel);
+        }else {
+            mBuilder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                    .setSmallIcon(R.drawable.ic_alarm_on_white_24dp)
+                    .setContentTitle(context.getResources().getString(R.string.app_name))
+                    .setTicker(mTitle)
+                    .setContentText(mTitle)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setContentIntent(mClick)
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true);
+
+
+
+        }
+
+        nManager.notify(mReceivedID, mBuilder.build());*/
+        // Create Notification
+
+    }
+
+    public void setAlarm(Context context, Calendar calendar, int ID) {
+        mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        // Put Reminder ID in Intent Extra
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra(ReminderEditActivity.EXTRA_REMINDER_ID, Integer.toString(ID));
+        mPendingIntent = PendingIntent.getBroadcast(context, ID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Calculate notification time
+        Calendar c = Calendar.getInstance();
+        long currentTime = c.getTimeInMillis();
+        long diffTime = calendar.getTimeInMillis() - currentTime;
+
+        // Start alarm using notification time
+        mAlarmManager.set(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + diffTime,
+                mPendingIntent);
+
+        // Restart alarm if device is rebooted
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    public void setRepeatAlarm(Context context, Calendar calendar, int ID, long RepeatTime) {
+        mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        // Put Reminder ID in Intent Extra
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra(ReminderEditActivity.EXTRA_REMINDER_ID, Integer.toString(ID));
+        mPendingIntent = PendingIntent.getBroadcast(context, ID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Calculate notification timein
+        Calendar c = Calendar.getInstance();
+        long currentTime = c.getTimeInMillis();
+        long diffTime = calendar.getTimeInMillis() - currentTime;
+
+        // Start alarm using initial notification time and repeat interval time
+        mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + diffTime,
+                RepeatTime, mPendingIntent);
+
+        // Restart alarm if device is rebooted
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    public void cancelAlarm(Context context, int ID) {
+        mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        // Cancel Alarm using Reminder ID
+        mPendingIntent = PendingIntent.getBroadcast(context, ID, new Intent(context, AlarmReceiver.class), 0);
+        mAlarmManager.cancel(mPendingIntent);
+
+        // Disable alarm
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+}
